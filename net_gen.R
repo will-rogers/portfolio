b8ec4dc10b097d663ec41237b2feb2e9652b2f69
Will Rogers <rogerswill47@gmail.com>
2026-01-18 16:26:16 -0500

    updated

--- net_gen.R
+++ net_gen.R
@@ -5,5 +5,5 @@ install_github("pablobarbera/scholarnetwork")
 library(scholarnetwork)
 d <- extractNetwork(id="TWc4vkkAAAAJ", n=500)
 str(d)
-plotNetwork(d$nodes, d$edges, file="/Users/willrogers/Downloads/portfolio/assets/network/network.html", 
-            fontsize = 15, width = 500, height = 500)
+plotNetwork(d$nodes, d$edges, file="/Users/willrogers/Downloads/portfolio/network.html", 
+            fontsize = 15, width = 1600, height = 6000)
